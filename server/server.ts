981bb28432dab221da079f4d26ffe34f66a49ae2
Nic Lo <nicolaloht@gmail.com>
2023-11-03 18:12:25 +0800

    test mj

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,15 @@
-nfgngiiongieioeergergi
+import express from "express";
+import { Request, Response } from "express";
+import path from "path";
+
+const app = express();
+
+app.get("/", function (req: Request, res: Response) {
+    res.end("Hello World");
+  });
+
+//Port
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}/`);
+});
