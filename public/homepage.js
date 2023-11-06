e126ac16229bf4f6e7bf8450c25183a10931f84b
Potato <seaweed724x@gmail.com>
2023-11-06 15:02:40 +0800

    remade frontend according to nico's figma

--- public/homepage.js
+++ public/homepage.js
@@ -1,13 +1,18 @@
 document.querySelectorAll(".players").forEach((element) => {
     element.addEventListener('click', async (e) => {
-    console.log("")
-    Swal.fire({
-        title: 'Sweet!',
-        text: 'Modal with a custom image.',
-        imageUrl: 'https://unsplash.it/400/200',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-      })
+    let {value: nameInput} = Swal.fire({
+      html: `
+      
+      `,
+      input: "text",
+      inputLabel: "Name",
+      showCancelButton: true,
+      inputValidator: (value) => {
+        if (!value) {
+          return "You need to write something!";
+        }
+      }
+    });
 });
 })
+
