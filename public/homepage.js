865281a037655a4b60be446bf0a7c06c6837b8bb
Potato <seaweed724x@gmail.com>
2023-11-06 17:26:15 +0800

    Revert "perfected"
    This reverts commit 9d9c99588ceae0da9e0a141efe473f68648455a8.

--- public/homepage.js
+++ public/homepage.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".players").forEach((element) => {
-  element.addEventListener('click', async (e) => {
-    let { value: nameInput } = Swal.fire({
+    element.addEventListener('click', async (e) => {
+    let {value: nameInput} = Swal.fire({
       html: `
       
       `,
@@ -13,41 +13,24 @@ document.querySelectorAll(".players").forEach((element) => {
         }
       }
     });
-  });
+});
 })
 
 const leaderBoard = document.querySelector(".leaderBoardBox")
 const home = document.querySelector(".box")
-const rightBtn = document.querySelector(".rightBtn")
-const cameraBtn = document.querySelector(".cameraBtn")
-const rightBtnText = document.querySelector(".rightBtn p")
-const rightBtnIcon = document.querySelector(".rightBtn i")
-const topBoxText = document.querySelector(".topBox p")
-let check = true
-// function clickedRightBtn(e) {
-//   console.log(e.target.id)
-  
-// }
+function clickedRightBtn(){
+  document.querySelector(".rightBtn").addEventListener("click", function(e){
+    console.log(e.target.id)
+    if (e.target.id === "leaderBoard"){
+      e.target.id = "home"
+      leaderBoard.style.display='flex';
+      home.style.display='none';
+    } else if (e.target.id === "home"){
+      e.target.id = "leaderBoard"
+      home.style.display='flex';
+      leaderBoard.style.display='none';
+    }
+  })
+}
 
-rightBtn.addEventListener("click", function(e){
-  console.log(check)
-  if (check) {
-    leaderBoard.style.display = 'flex';
-    home.style.display = 'none';
-    cameraBtn.style.display = 'none'
-    rightBtnText.innerHTML = "現時賽果"
-    rightBtnIcon.classList = "fa-solid fa-house"
-    topBoxText.innerHTML = "麻雀排行榜"
-    check = false
-
-  } else if (!check) {
-    home.style.display = 'flex';
-    leaderBoard.style.display = 'none';
-    cameraBtn.style.display = 'flex'
-    rightBtnText.innerHTML = "排行榜"
-    rightBtnIcon.classList = "fa-solid fa-ranking-star"
-    topBoxText.innerHTML = "麻雀賽果"
-    check = true
-  }
-})
-
+clickedRightBtn()
