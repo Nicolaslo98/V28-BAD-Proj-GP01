07cfbc039a7fc31fe44663f6c9fc6d60dc4f00d8
Potato <seaweed724x@gmail.com>
2023-11-06 16:17:13 +0800

    added leaderboard frontend

--- public/homepage.js
+++ public/homepage.js
@@ -16,3 +16,21 @@ document.querySelectorAll(".players").forEach((element) => {
 });
 })
 
+const leaderBoard = document.querySelector(".leaderBoardBox")
+const home = document.querySelector(".box")
+function clickedRightBtn(){
+  document.querySelector(".rightBtn").addEventListener("click", function(e){
+    console.log(e.target.id)
+    if (e.target.id === "leaderBoard"){
+      e.target.id = "home"
+      leaderBoard.style.display='flex';
+      home.style.display='none';
+    } else if (e.target.id === "home"){
+      e.target.id = "leaderBoard"
+      home.style.display='flex';
+      leaderBoard.style.display='none';
+    }
+  })
+}
+
+clickedRightBtn()
