6a2db827f7d04e303cc4c6b4ed61329ea1f454fb
ryanlee1001 <ryanlee1001@gmail.com>
2023-11-06 14:44:17 +0800

    Ryan first commit

--- server/server.ts
+++ server/server.ts
@@ -3,13 +3,11 @@ import dotenv from "dotenv";
 import { sessionMiddleware } from "./session";
 import { Request, Response } from "express";
 import path from "path";
+import fs from 'fs';
 
 dotenv.config();
 const app = express();
 
-app.get("/", function (req: Request, res: Response) {
-  res.end("Hello World");
-});
 
 //Request Log
 app.use((req, res, next) => {
@@ -19,11 +17,52 @@ app.use((req, res, next) => {
 
 //Third party middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(sessionMiddleware);
+
+
+//
+
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+// Endpoint to handle the save image request
+app.post('/', (req, res) => {
+  const imageData = req.body.data; // Assuming you have a body parser middleware to parse the request body
+
+  // Specify the path to the folder where the images will be saved
+  const folderPath = path.join(__dirname, 'photo');
+
+  // Create the folder if it doesn't exist
+  // if (!fs.existsSync(folderPath)) {
+  //   fs.mkdirSync(folderPath);
+  // }
+
+  // Generate a unique filename for the image
+  const filename = `image_${Date.now()}.png`;
+
+  // Save the image data to the folder
+  console.log(imageData)
+  const filePath = path.join(folderPath, filename);
+  fs.writeFileSync(filePath, imageData, 'base64');
+
+  res.sendStatus(200);
+});
+
+
+
+
+
+
+
 
 //Port
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}/`);
 });
+
