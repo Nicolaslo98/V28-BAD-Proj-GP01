06ab676bdd138e2f49f6432695798da4fbf61d1b
Potato <seaweed724x@gmail.com>
2023-11-08 12:38:50 +0800

    final fixed for back camera

--- public/homepage.js
+++ public/homepage.js
@@ -83,7 +83,7 @@ function genCamera(){
         const webcam = devices.find(device => device.kind === 'videoinput');
         if (webcam) {
           // Access the webcam
-          const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: webcam.deviceId, facingMode: 'environment'}});
+          const stream = await navigator.mediaDevices.getUserMedia({ video: { deviceId: webcam.deviceId, facingMode: {exact: 'environment'}}});
           // Set the video source
           video.srcObject = stream;
         } else {
