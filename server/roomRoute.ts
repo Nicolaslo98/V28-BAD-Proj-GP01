fd841dd8cff18e14ae047a4d2e4be2664bb81ff6
nic lo <nicolaloht@gmail.com>
2023-11-09 16:56:44 +0800

    room function backend v2

--- server/roomRoute.ts
+++ server/roomRoute.ts
@@ -2,6 +2,6 @@ import express, { Request, Response } from "express";
 import { roomController } from "./server";
 export const roomRoutes = express.Router();
 
-roomRoutes.post("/create",roomController.checkRoom)//Method: POST '/api/room
+roomRoutes.post("/",roomController.checkRoom)//Method: POST '/api/room
 roomRoutes.post("/create",roomController.createRoom)//Method: POST '/api/room/create
 roomRoutes.post("/join", roomController.joinRoom)//Method: POST '/api/room/join
