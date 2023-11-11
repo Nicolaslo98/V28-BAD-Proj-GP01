43bbb4216328c16f8656de03f99394ab381f1e7d
ryanlee1001 <ryanlee1001@gmail.com>
2023-11-11 10:17:55 +0800

    added game in database round

--- service/rank-service.ts
+++ service/rank-service.ts
@@ -8,6 +8,7 @@ export class RankService {
     const rankData = await this.knex('player')
     .select('username', 'user_total_score')
     .where('room_id', room_id)
+    .orderBy('user_total_score', 'desc')
     return rankData;
   }
 }
